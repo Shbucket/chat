319d502e43688d14bdacec4706ffa44a120f534e
shoamri joseph <shomarijoseph@gmail.com>
2022-12-27 14:45:25 -0500

    + upload images to firebase function

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
 
 import {getAuth} from 'firebase/auth'
 const firebaseConfig = {
@@ -16,4 +17,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const auth = getAuth()
+export const auth = getAuth();
+export const storage = getStorage();
