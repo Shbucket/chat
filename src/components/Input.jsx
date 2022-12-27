203f8ef975be445657712da13f86dfc52f5bbd4a
shoamri joseph <shomarijoseph@gmail.com>
2022-12-26 20:49:43 -0500

    added styling to chat section

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faImage, faPaperclip } from "@fortawesome/free-solid-svg-icons";
+
+const image = <FontAwesomeIcon icon={faImage} />
+const clip = <FontAwesomeIcon icon={faPaperclip} />
 
 export const Input = () => {
   return (
-    <div className='input'>Input</div>
-  )
-}
- export default Input
+    <div className="input">
+      <input type="text" placeholder="Type something..." />
+      <div className="send">
+        <span>{clip}</span>
+        <input type="file" style={{ display: "none" }} id="file" />
+        <label htmlFor="file"><span>{image}</span></label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
+};
+export default Input;
